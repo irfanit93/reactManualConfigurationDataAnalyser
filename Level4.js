c1a7603131bbfde6bc6f60ccd440bf2cabdcc57e
irfanit93 <irfanit93@gmail.com>
2017-11-10 23:15:25 +0530

    propsandstatev1

--- Level4.js
+++ Level4.js
@@ -16,26 +16,15 @@ this.setState({num3:this.state.num3+this.props.num3});
 this.setState({num4:this.state.num4+this.props.num4});
 this.setState({num5:this.state.num5+this.props.num5});
 */
-if(this.props.updatestatus=="yes")
-{
-this.setState((prevState,props)=>({num1:prevState.num1+props.num1}));
-this.setState((prevState,props)=>({num2:prevState.num2+props.num2}));
-this.setState((prevState,props)=>({num3:prevState.num3+props.num3}));
-this.setState((prevState,props)=>({num4:prevState.num4+props.num4}));
-this.setState((prevState,props)=>({num5:prevState.num5+props.num5}));
-}
-else if(this.props.updatestatus=="no")
-{
-this.setState((prevState,props)=>({num1:prevState.num1-props.num1}));
-this.setState((prevState,props)=>({num2:prevState.num2-props.num2}));
-this.setState((prevState,props)=>({num3:prevState.num3-props.num3}));
-this.setState((prevState,props)=>({num4:prevState.num4-props.num4}));
-this.setState((prevState,props)=>({num5:prevState.num5-props.num5}));
-}
+        this.setState((prevState,props)=>({num1:prevState.num1+props.num1}));
+        this.setState((prevState,props)=>({num2:prevState.num2+props.num2}));
+        this.setState((prevState,props)=>({num3:prevState.num3+props.num3}));
+        this.setState((prevState,props)=>({num4:prevState.num4+props.num4}));
+        this.setState((prevState,props)=>({num5:prevState.num5+props.num5}));
 }
 render()
 {
-const nums={num1:this.state.num1,num2:this.state.num2,num3:this.state.num3,num4:this.state.num4,num5:this.state.num5,updatestatus:this.props.updatestatus};
+const nums={num1:this.state.num1,num2:this.state.num2,num3:this.state.num3,num4:this.state.num4,num5:this.state.num5};
 return (<div>
 <div>Level4TotalSubscribers
 <span style={{margin:'3%'}}>{this.state.num1}</span>
--- Unsubscribe.js
+++ Unsubscribe.js
@@ -6,39 +6,19 @@ class TotalSubscribers extends React.Component
 constructor(props)
 {
 super(props);
-this.state={num1:1,num2:2,num3:3,num4:4,num5:5,updatestatus:"yes"};
+this.state={num1:1,num2:2,num3:3,num4:4,num5:5};
 }
 updatenum()
 {
-this.setState({updatestatus:"yes"});
 this.setState({num1:++this.state.num1});
 this.setState({num2:++this.state.num2});
 this.setState({num3:++this.state.num3});
 this.setState({num4:++this.state.num4});
 this.setState({num5:++this.state.num5});
-alert("UpdateStatus "+this.state.updatestatus);
-}
-undonum()
-{
-this.setState({updatestatus:"no"});
-this.setState({num1:--this.state.num1});
-this.setState({num2:--this.state.num2});
-this.setState({num3:--this.state.num3});
-this.setState({num4:--this.state.num4});
-this.setState({num5:--this.state.num5});
-alert("UndoStatus "+this.state.updatestatus);
-}
-componentWillUpdate()
-{
-alert("UpdatedUndo Status "+this.state.updatestatus);
-}
-componentDidUpdate()
-{
-alert("UpdatedUndo Status Completed "+this.state.updatestatus);
 }
 render()
 {
-const nums={num1:this.state.num1,num2:this.state.num2,num3:this.state.num3,num4:this.state.num4,num5:this.state.num5,updatestatus:this.state.updatestatus};
+const nums={num1:this.state.num1,num2:this.state.num2,num3:this.state.num3,num4:this.state.num4,num5:this.state.num5};
 return (<div style={{border:'1px solid blue'}}>
 <div>TopLvlTotalSubscribers
 <span style={{margin:'3%'}}>{this.state.num1}</span>
@@ -48,7 +28,6 @@ return (<div style={{border:'1px solid blue'}}>
 <span style={{margin:'3%'}}>{this.state.num5}</span>
 <Level1 {...nums}/>
 <button type="button" onClick={this.updatenum.bind(this)}>UpdateNum</button>
-<button type="button" onClick={this.undonum.bind(this)}>UndoNum</button>
 </div>
 </div>);
 }
